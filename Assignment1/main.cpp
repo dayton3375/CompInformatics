2ae03493932d78bac489649924c0f3e24f79e9c2
dayton3375 <dayton.dekam@wsu.edu>
2022-02-10 17:39:02 -0800

    Validate cmd args and stored values

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -6,6 +6,24 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    // make sure there's enough parameters
+    if (argc < 3 || argc > 4)
+    {
+        cout << "Invalid # of cmd arguments: " << argc - 1 << " were given. 2 are needed" << endl;    
+        cout << "try: <executable name> <input file containing both s1 and s2> <0: global, 1: local> <optional: path to parameters config file>" << endl;
+        return -1;
+    }
+
+    // gather program arguments
+    string inputFileName = argv[1];
+    char seqType = argv[2][0];
+
+    // if user included the optional argument of a param file, otherwise default file
+    string paramFileName = "parameters.config";
+    if (argc == 4)
+    {
+        paramFileName = argv[4];
+    }
 
     return 0;
 }
